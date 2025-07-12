5248633c50c8f7549462cb6a088bb6df4752d268
vamsipamu <venketramana.vamsi@gmail.com>
2025-07-12 18:02:00 +0530

    Update directoryexist.sh

--- directoryexist.sh
+++ directoryexist.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-DIR=rathi
+DIR=vamsi
 if [ -d "$DIR" ]; then
 echo "$DIR exists"
+else
+echo "$DIR does not exist"
+fi
+
+#list the files in a dir if dir exists
+
+#!/bin/bash
+DIR=rathi
+if [ -d "$DIR" ]; then
+echo "$DIR exists and files in a directory is $(ls -l $dir)" 
 else
 echo "$DIR does not exist"
 fi
