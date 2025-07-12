66292f433db09287cfa40f16be0dc667e6ecd3fe
vamsipamu <venketramana.vamsi@gmail.com>
2025-07-12 16:32:44 +0530

    Update and rename archivelogs.sh to find.sh

--- find.sh
+++ find.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #fine is the command to check logs or files which are modified, accessed, and changed(atime,ctime7mtime)
 find /var/log -type f -mtime +7 -exec tar -rvf old_logs.tar {} \: -exec rm {} \;
+
+#top10 largest file
+
+find ./ -type f -exec du -h {} + | sort -rh | head -n 10
+
